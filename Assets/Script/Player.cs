ef52e4370314aad62888e1df721bfaa08bcfa292
Alicetear <phrnakanth@gmail.com>
2025-12-02 15:38:55 +0700

    Edit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] public float moveSpeed = 5f;
+    [SerializeField] public bool IsInvuInerable = false;
+    private float jumpForce = 10f;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void SetMoveSpeed(float speed)
+    {
+
+    }
+
+    public void SetInvulnerabillity(bool isEnabled)
+    {
+        Debug.Log("isEnabled Check"); //ค่าเปลี่ยน
+    }
+
+    private void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        rb.linearVelocity = new Vector2 (moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
